960c10fd79506768c23e6e0e660b347117e55dba
Fabio <fabio@ifabio.com.br>
2022-11-03 20:42:14 -0300

    nfIF

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -9,10 +9,17 @@ export class DataBidingComponent implements OnInit {
 
   public nome: string = "Fabio";
   public idade: number = 40;
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png";
+  public imgTitle: string = "Property Biding";
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public alertaInfo(){
+    alert("Deus bom !");
+
   }
 
 }
