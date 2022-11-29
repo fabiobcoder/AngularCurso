84b9d44a806bd5a4529b69fafde1cb320ba0c550
FABIO BARBOSA <31438903+fabiobcoder@users.noreply.github.com>
2022-11-28 21:15:42 -0300

    Angular Input

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
