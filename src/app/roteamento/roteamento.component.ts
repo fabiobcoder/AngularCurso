0ccff6d29bbf1c1e3454b50cd5f0173f5bbf74eb
FABIO BARBOSA <31438903+fabiobcoder@users.noreply.github.com>
2022-12-01 00:00:25 -0300

    Roteamento

---
+++ src/app/roteamento/roteamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roteamento',
+  templateUrl: './roteamento.component.html',
+  styleUrls: ['./roteamento.component.scss']
+})
+export class RoteamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
