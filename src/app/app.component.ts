6d4ecdafd56ea1d464cbbff95d5026c54ff51dc9
FABIO BARBOSA <31438903+fabiobcoder@users.noreply.github.com>
2022-12-05 23:07:14 -0300

    Alteração na Interface e CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <app-input [contador]="addValue"></app-input>
-
-  <br>
-  <button (click)="add()")> Add </button>
-
-  <router-outlet></router-outlet>
-  `,
-
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit{
-
-  public destruir: boolean = true;
-
-  public addValue: number = 10;
-
-  constructor() {
-
-  }
-ngOnInit(): void {
-}
-public add(){
-  this.addValue += 1;
-}
-public destruirComponent(){
-  this.destruir = false;
-}
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { DataBidingComponent } from './data-biding/data-biding.component';
-import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
-import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
-import { SharedModule } from "./shared/shared.module";
-import { FormComponent } from './form/form.component';
-import { RotasComponent } from './rotas/rotas.component';
-import { RoteamentoComponent } from './roteamento/roteamento.component';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        TitleComponent,
-        DataBidingComponent,
-        DiretivasEstruturaisComponent,
-        DiretivasAtributosComponent,
-        FormComponent,
-        RotasComponent,
-        RoteamentoComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule
-    ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
