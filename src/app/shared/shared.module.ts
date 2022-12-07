428484a87e358adb3ef166d9a1e732ba6811f28c
FABIO BARBOSA <31438903+fabiobcoder@users.noreply.github.com>
2022-12-07 12:44:22 -0300

    Criação de Ifood Service.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import { FoodAddComponent } from './food-add/food-add.component';
+import { FoodListComponent } from './food-list/food-list.component';
 
 
 
@@ -10,11 +14,16 @@ import { OutputComponent } from './output/output.component';
   declarations: [
     NewComponentComponent,
     InputComponent,
-    OutputComponent
+    OutputComponent,
+    FoodListComponent,
+    FoodAddComponent
   ],
   exports:[
     NewComponentComponent,
-    InputComponent
+    InputComponent,
+    OutputComponent,
+    FoodListComponent,
+    FoodAddComponent
   ],
   imports: [
     CommonModule
